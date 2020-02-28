1d60c5eaf62127cd8fcab2f6699a645da1915565
Rodianov <rodianov@MacBook-Pro-Rodianov.local>
2020-02-28 17:36:00 +0300

    Initial commit

---
+++ Course2Week5Task2/ViewController1.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Course2Week5Task2
+//
+//  Created by Rodianov on 28.02.2020.
+//  Copyright © 2020 Rodionov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Course2Week5Task2/ViewController2.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController2.swift
+//  Course2Week5Task2
+//
+//  Created by Rodianov on 28.02.2020.
+//  Copyright © 2020 Rodionov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
